6658c9fa1dbb84571c8863a79af2dc370fac9ab9
Chris Hansson <chansson@icloud.com>
2013-10-13 07:52:00 -0700

    Initial commit, as framework

---
+++ obj-mysql/obj_mysql.h
@@ -0,0 +1,13 @@
+//
+//  obj_mysql.h
+//  obj-mysql
+//
+//  Created by Christian Hansson on 10/13/13.
+//  Copyright (c) 2013 ipluris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface obj_mysql : NSObject
+
+@end
---
+++ obj-mysql/obj_mysql.m
@@ -0,0 +1,13 @@
+//
+//  obj_mysql.m
+//  obj-mysql
+//
+//  Created by Christian Hansson on 10/13/13.
+//  Copyright (c) 2013 ipluris. All rights reserved.
+//
+
+#import "obj_mysql.h"
+
+@implementation obj_mysql
+
+@end
